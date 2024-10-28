51a1019f7026ec1ca6db827fcb41e8ef9d1b8c2d
Likhitha Boorsu <twinkleboorsu1717@gmail.com>
2024-10-28 18:41:38 +0530

    addingfiles

--- src/main/java/com/qualizeal/App.java
+++
@@ -1,9 +0,0 @@
-package com.qualizeal;
-
-import com.qualizeal.javaapp06.operation.FibonacciSeries;
-public class App {
-	public static void main(String[] args) {
-		FibonacciSeries fibonacciSeries=new FibonacciSeries();
-		
-	}
-}
