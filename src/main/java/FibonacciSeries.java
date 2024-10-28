a0264cd1a50c9fb27a31d2ad6810add07302311d
Sri Harika <nookalasriharika@gmail.com>
2024-10-28 18:03:32 +0530

    Fibonnaci

--- src/main/java/FibonacciSeries.java
+++ src/main/java/FibonacciSeries.java
@@ -1,29 +1,15 @@
 package com.qualizeal.javaapp06.operation;
-public class FibonacciSeries{
-	public String defineFibonacciSeries(){
-      return "FibonacciSeries";
-		
-    }
-	public String generateFibonacciSeries(int num1){
-		if(num1 < 0){
-			return "-1";
-		}
-		
-		else if(num1 == 0){
-			return "-2";
-		}
-		
-		else{
-			String result="0";
-			int first=0,second=1;
-			while(second <= num1){
-				result=result+", "+second;
-				int next=first+second;
-				first=second;
-				second=next;
-			}
-			return result;
-		}
-		
+
+public class FibonacciSeries {
+	public String defineFibonacciSeries() {
+		return "Fibonacci series";
 	}
+	
+	public int generateFibonacciSeries(int n) {
+	        if (n <= 1) {
+	            return n;
+	        }
+	        return generateFibonacciSeries(n - 1) + generateFibonacciSeries(n - 2);
+	    }
 }
+	    
