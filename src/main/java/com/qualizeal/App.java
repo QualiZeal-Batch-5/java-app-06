462671efcdad511d01c23579fca95fda8772956b
Disney Pallavi Bezawada <disneypallavi@gmail.com>
2024-10-28 15:53:51 +0530

    Added files.

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,15 @@
 package com.qualizeal;
 
+import com.qualizeal.javaapp06.operation.FibonacciSeries;
+
 public class App {
 	public static void main(String[] args) {
+		FibonacciSeries fibonacciSeries = new FibonacciSeries();
+		
+		System.out.println(fibonacciSeries.defineFibonacciSeries());
+		
+		System.out.println(fibonacciSeries.generateFibonacciSeries(50));
+		 
 		
 	}
 }
---
+++ src/main/java/com/qualizeal/javaapp06/operation/FibonacciSeries.java
@@ -0,0 +1,41 @@
+package com.qualizeal.javaapp06.operation;
+
+public class FibonacciSeries {
+	
+	public String defineFibonacciSeries() {
+		
+		return "The Fibonacci sequence is a type series where each number is the sum of the two that precede it. It starts from 0 and 1 usually";
+	}
+	
+	public String generateFibonacciSeries(int num1) {
+		
+		if(num1 < 0) {
+			
+			return "-1";
+		}
+		
+		else if(num1 == 0) {
+			
+			return "-2";
+		}
+		
+		else {
+			
+			String result = "0, 1";
+			int a=0,b=1,i=2,c=0;
+			int temp=num1;
+			
+			while((c+a) <=temp) {
+				c=a+b;
+				a=b;
+				b=c;
+				i=i+1;
+				
+				result = result+','+' '+c;
+			}
+			
+			return result;
+		}
+	}
+
+}
