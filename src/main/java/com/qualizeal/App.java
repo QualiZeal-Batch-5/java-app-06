8066b001c67f03c051f15949b604df5e5bd4ca0c
Dandu Srija <dandusrija2503@gmail.com>
2024-10-28 18:18:32 +0530

    Performed an operation to generate FIBONACCI SERIES starting from 0 to a particular number.

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,9 +1,12 @@
 package com.qualizeal;
-
+ 
 import com.qualizeal.javaapp06.operation.FibonacciSeries;
+ 
 public class App {
 	public static void main(String[] args) {
-		FibonacciSeries fibonacciSeries=new FibonacciSeries();
-		
+		FibonacciSeries fibonacciSeries = new FibonacciSeries();
+	
+		String result = fibonacciSeries.generateFibonacciSeries(6);
+		System.out.print(result);
 	}
 }
