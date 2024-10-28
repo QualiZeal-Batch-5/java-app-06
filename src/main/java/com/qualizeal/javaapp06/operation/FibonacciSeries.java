c725a4cb4f149e7579365728fb7acb17218e325e
Priyanka Kalyampudi <kalyampudipriyanka@gmail.com>
2024-10-28 15:55:34 +0530

    Added Solution

---
+++ src/main/java/com/qualizeal/javaapp06/operation/FibonacciSeries.java
@@ -0,0 +1,31 @@
+package com.qualizeal.javaapp06.operation;
+
+public class FibonacciSeries {
+	public String defineFibonacciSeries() {
+		return "A Fibonacci number is part of a sequence where each number is the sum of the two preceding ones.";
+	}
+
+	public String generateFibonacciSeries(int num) {
+		if (num < 0) {
+			return "-1";
+		} else if (num == 0) {
+			return "-2";
+
+		} else {
+			String result = "0, 1";
+			int a = 0, b = 1, i = 2, c = 0;
+			int temp = num;
+			while ((c + a) <= temp) {
+				c = a + b;
+				a = b;
+				b = c;
+				i = i + 1;
+				result = result + ',' + ' ' + c;
+			}
+			return result;
+
+		}
+
+	}
+
+}
