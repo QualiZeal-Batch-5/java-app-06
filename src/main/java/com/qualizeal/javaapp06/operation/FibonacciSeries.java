03659bfe3a92891973f9633f32d2dfab700b3d34
Aruna Kumari Pattapu <arunakumarip206@gmail.com>
2024-10-28 17:58:10 +0530

    Solution fibinocci.

---
+++ src/main/java/com/qualizeal/javaapp06/operation/FibonacciSeries.java
@@ -0,0 +1,31 @@
+package com.qualizeal.javaapp06.operation;
+
+public class FibonacciSeries {
+	
+	public String defineFibonacciSeries() {
+		return "Fibonacci sequence is a series of numbers where each number is the sum of the two numbers that come before it.";
+	}
+	
+	public String generateFibonacciSeries(int number) {
+		if(number < 0) {
+			return "-1";
+		}
+		else if(number == 0) {
+			return "-2";
+		}
+			else {
+				String result = "0, 1";
+				int a=0,b=1,i=2,c=0;
+				int temp=number;
+				while((c+a) <=temp) {
+					c=a+b;
+					a=b;
+					b=c;
+					i=i+1;
+					result = result+','+' '+c;
+					
+			}
+				return result;
+		}
+	}
+}
